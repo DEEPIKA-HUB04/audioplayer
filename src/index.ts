e4b6db4a4315ffc41eae005d5aac6a10e11df8e2
DEEPIKA-HUB04 <66515582+DEEPIKA-HUB04@users.noreply.github.com>
2020-08-21 17:18:25 +0530

    Add files via upload

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import { AudioPlayer } from "./audio";
+
+const player = new AudioPlayer('.audioPlayer', [
+
+    { link : 'mp3/Kalimba.mp3', fileName : "Kalimba"},
+    { link : 'mp3/samlemp3.mp3', fileName : "Samle"},
+    { link : 'mp3/SoundHelix-Song-1.mp3', fileName : "SoundHelix-Song-1"},
+    { link : 'mp3/SoundHelix-Song-15.mp3', fileName : "SoundHelix-Song-15"},
+    { link : 'mp3/SoundHelix-Song-7.mp3', fileName : "SoundHelix-Song-7"}  
+    
+]);
